d1d16c0dc8ca7de84af310c7d0ecb5884b87ca18
Jack Pearkes <jackpearkes@gmail.com>
2017-09-18 10:50:29 -0500

    subnet: do not map a public IP on launch

--- main.tf
+++ main.tf
@@ -24,7 +24,6 @@ resource "aws_route" "internet_access" {
 resource "aws_subnet" "default" {
   vpc_id                  = "${aws_vpc.default.id}"
   cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
 }
 
 # A security group for the ELB so it is accessible via the web
